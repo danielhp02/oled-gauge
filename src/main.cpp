76e2d8b3e7a1f1123d875b8e8782553630f8f87a
Daniel Hewitt-Perraud <daniel.hewitt-perraud@monashmotorsport.com>
2026-05-03 14:29:57 +1000

    Ported basic button counting functionality and display to ESP32-C3

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 // Display docs: https://github.com/olikraus/u8g2/wiki
 // Display wired up as: https://media.jaycar.com.au/product/resources/XC3728_manualMain_92743.pdf
 
-
 // Font Reference:
 // - u8g2_font_scrum_tr: nice general font, 15px tall
 // - u8g2_font_inr30_mr: inconsolata, 30px tall (also 16, 19, 21, 24, 27)
@@ -14,21 +13,42 @@
 #include <SPI.h>
 #include <Wire.h>
 
-// Setup SPI pins
-#define SPI_CLK   13
-#define SPI_DATA  11
-#define SPI_CS    10
-#define SPI_DC    9
+// Setup I/O pins
+#if defined(CONFIG_IDF_TARGET_ESP32C3)
+    // ESP32 C3 pins
+    // SPI
+    constexpr int spi_clk   = SCK;       // 4
+    constexpr int spi_data  = MOSI;      // 6
+    constexpr int spi_rst   = 9;
+    constexpr int spi_cs    = SS;        // 7
+    constexpr int spi_dc    = 8;
 
-// Setup input pins
-#define SW_PIN 2
+    // Input
+    constexpr int sw_pin = 3;
 
-// Note on rotation: 
+#elif defined(ARDUINO_AVR_UNO)
+    // Arduino Uno pins (for testing)
+    // SPI
+    constexpr int spi_clk = 13;
+    constexpr int spi_data = 11;
+    constexpr int spi_rst = 1;
+    constexpr int spi_cs = 10;
+    constexpr int spi_dc = 9;
+
+    // Input
+    constexpr int sw_pin = 2;
+
+#else
+    // undefined board, throw an error
+    #error "Untested board. Please define pins."
+#endif
+
+// Note on rotation:
 //  - U8G2_R0: pins up
-//  - U8G2_R2
+//  - U8G2_R2: pins down
 
 // Create display object
-U8G2_SH1106_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R2, SPI_CS, SPI_DC);
+U8G2_SH1106_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R2, spi_cs, spi_dc, spi_rst);
 
 // Variables to track button presses
 int button_state = 0;
@@ -38,41 +58,50 @@ char counter_text[20];
 
 const char *heading = "button";
 
-void setup(void) {
-  // Initialise display
-  u8g2.begin();
+void setup(void)
+{
+    // Initialise display
+    u8g2.begin();
 
-  // Initialise button
-  pinMode(SW_PIN, INPUT);
+    // Initialise serial
+    Serial.begin(115200);
+
+    // Initialise button
+    pinMode(sw_pin, INPUT);
 }
 
-void loop(void) {
-  u8g2.firstPage();
-  do {
-    // Set heading
-    u8g2.setFont(u8g2_font_inr16_mr);
-    u8g2.setFontPosTop();
-    u8g2.drawStr(64-u8g2.getStrWidth(heading)/2, 0, heading);
+void loop(void)
+{
+    u8g2.firstPage();
+    do
+    {
+        // Set heading
+        u8g2.setFont(u8g2_font_inr16_mr);
+        u8g2.setFontPosTop();
+        u8g2.drawStr(64 - u8g2.getStrWidth(heading) / 2, 0, heading);
 
-    // check for button press
-    button_state = digitalRead(SW_PIN);
-    if (button_state != last_button_state) {
-      if (button_state == HIGH) { // rising edge
-        counter++;
-      }
-      delay(15); // debounce
-    }
+        // check for button press
+        button_state = digitalRead(sw_pin);
+        if (button_state != last_button_state)
+        {
+            if (button_state == HIGH)
+            { // rising edge
+                counter++;
+                Serial.println("Button pressed!");
+            }
+            delay(15); // debounce
+        }
 
-    last_button_state = button_state;
+        last_button_state = button_state;
 
-    // counter to text
-    sprintf(counter_text, "%d", counter);
+        // counter to text
+        sprintf(counter_text, "%d", counter);
 
-    u8g2.setFont(u8g2_font_inr30_mr);
-    u8g2.setFontPosBottom();
-    // u8g2.drawStr(0,30,"Hello World!");
+        u8g2.setFont(u8g2_font_inr30_mr);
+        u8g2.setFontPosBottom();
+        // u8g2.drawStr(0,30,"Hello World!");
 
-    // Render number of keypresses centre aligned horizontally
-    u8g2.drawStr(64-u8g2.getStrWidth(counter_text)/2,64,counter_text);
-  } while ( u8g2.nextPage() );
+        // Render number of keypresses centre aligned horizontally
+        u8g2.drawStr(64 - u8g2.getStrWidth(counter_text) / 2, 64, counter_text);
+    } while (u8g2.nextPage());
 }
